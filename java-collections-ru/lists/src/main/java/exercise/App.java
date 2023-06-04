3fab64477444b6f12d887cd97df8043066ffee5b
@hexlet/cli <support@hexlet.io>
2023-06-04 21:31:06 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -3,16 +3,30 @@ package exercise;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 // BEGIN
 public class App {
     public static boolean scrabble(String randomStr, String natureStr) {
-        List<Character> randomChrs = new ArrayList<Character>();
-        for(int i = 0; i < randomStr.length(); i++){
-            randomChrs.add(randomStr.charAt(i));
+        if (randomStr == null || natureStr == null || randomStr.length() < natureStr.length()) {
+            return false;
+        }
+        randomStr = randomStr.toLowerCase();
+        natureStr = natureStr.toLowerCase();
+        List<Character> listRandChrs = new ArrayList<Character>();
+        for (int i = 0; i < randomStr.length(); i++) {
+            listRandChrs.add(randomStr.charAt(i));
+        }
+        for (int i = 0; i < natureStr.length(); i++) {
+            int findIndex = listRandChrs.indexOf(natureStr.charAt(i));
+            if (findIndex != -1) {
+                listRandChrs.remove(findIndex);
+            }
+            else {
+                return false;
+            }
         }
         return true;
-
     }
 }
 //END
