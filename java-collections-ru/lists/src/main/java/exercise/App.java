e5216272a47a3a7fe16ac3fe752b80d934d21652
@hexlet/cli <support@hexlet.io>
2023-06-04 21:16:56 +0300

    submit java-collections-ru/lists

---
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -0,0 +1,18 @@
+package exercise;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+// BEGIN
+public class App {
+    public static boolean scrabble(String randomStr, String natureStr) {
+        List<Character> randomChrs = new ArrayList<Character>();
+        for(int i = 0; i < randomStr.length(); i++){
+            randomChrs.add(randomStr.charAt(i));
+        }
+        return true;
+
+    }
+}
+//END
